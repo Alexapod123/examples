e7f7acabbe539685f088b230e7d0480bd812c8c3
Alexa <alex_krim99@mail.ru>
2023-03-19 22:08:48 +0300

    task 2

---
+++ seminar_1/task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число №1");
+int numberA = Convert.ToInt32( Console.ReadLine() );
+Console.WriteLine("Введите целое число №2");
+int numberB = Convert.ToInt32( Console.ReadLine() );
+if (numberA / numberB == numberB)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
