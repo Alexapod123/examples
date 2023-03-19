5f201d11650b2fd934fc3ac3e8315fa506f6688f
Alexa <alex_krim99@mail.ru>
2023-03-19 23:22:06 +0300

    Выполнили задачу с промежутком

---
+++ seminar_1/task_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает одно целое число N, на выходе показывает все числа в промежутке от -N до N.
+
+Console.WriteLine("Введите целое число");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int count = -(number);
+        while (count <= number)
+        {
+            Console.Write(count);
+            count = count + 1;
+        }
