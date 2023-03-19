7873e6e5c03969dd2cd469da11e79bb55b957958
Alexa <alex_krim99@mail.ru>
2023-03-19 21:57:52 +0300

    task 1

---
+++ seminar_1/task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа для возведения в квадрат
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32( Console.ReadLine() );
+int square = number * number;
+Console.WriteLine("Квадрат введенного числа равен " + square);
+
+if (number<0)
+{
+    Console.WriteLine("Введено отрицательное число");
+}
+else
+{
+    Console.WriteLine("Введено неотрицательное число");
+}
